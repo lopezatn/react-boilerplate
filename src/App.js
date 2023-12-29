6228d5714a1c0d43322a54fd8c919516d292dd20
lopezatn <agustinlpzb@gmail.com>
2023-12-29 15:19:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        Welcome to React App thats build using Webpack and Babel separately
+      </h1>
+    </div>
+  );
+};
+
+export default App;
